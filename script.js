3ca0e6bdf20f5fe7260f9a3ddaa4a85fca0a43fd
Suruthika Ramesh <manimala1614@gmail.com>
2026-06-30 14:30:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ const result=[];
 for(let i=0;i<str.length;i+=size){
 result.push(str.slice(i,i+size));}
 
-
-
 }
 return result;
 
 
+
 // Do not change the code below
-//const str = prompt("Enter String.");
-//const size = prompt("Enter Chunk Size.");
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
